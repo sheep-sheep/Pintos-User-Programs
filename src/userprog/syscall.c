e4eb0e329da4b7b15a37483380f4f9982d695012
Yang <zhang.yang.frank@gmail.com>
2015-04-21 16:59:57 -0400

    Validate ptr while accessing user memroy.

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -3,6 +3,8 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "threads/vaddr.h"
+#include "userprog/pagedir.h"
 
 static void syscall_handler (struct intr_frame *);
 
@@ -17,4 +19,19 @@ syscall_handler (struct intr_frame *f UNUSED)
 {
   printf ("system call!\n");
   thread_exit ();
+}
+
+int user_provide_ptr(const void *vaddr){
+	// Verify the validity of a user-provided pointer
+	if (!is_user_vaddr(vaddr)){
+  	thread_exit();
+  	return 0;
+  }
+
+  void *ptr = pagedir_get_page(thread_current()->pagedir, vaddr);
+  if (!ptr){
+  	thread_exit();
+  	return 0; // Double check whether we could get right pointer address
+  }
+  return (int) ptr;
 }
